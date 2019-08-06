1dd8fbbdd936a0f0530921a7bc5516377cf0596b
Arjun <arjunk@thoughtworks.com>
2019-08-06 15:51:49 +0530

    0000 | Showing vaccination

--- child/checklistRules.js
+++ child/checklistRules.js
@@ -10,6 +10,7 @@ import {
 
 const ViewFilter = RuleFactory("d712105c-14d9-4c64-b5e0-6150cf88f7a2", "ViewFilter");
 const WithStatusBuilder = StatusBuilderAnnotationFactory('checklistItem', 'formElement');
+const EnrolmentChecklists = RuleFactory("d440e0b4-a404-4bdf-ba5e-f1e5d5cf5cc0", "Checklists");
 
 @ViewFilter("54501bb9-43e9-4b32-aea5-59358d3832a2", "Chetna Check List View Filter", 100.0, {})
 class CheckListViewFilterChetna {
@@ -26,5 +27,22 @@ class CheckListViewFilterChetna {
 
 }
 
+@EnrolmentChecklists("fae40f1f-3669-452f-ab99-b5323346ef4c", "Child vaccination schedule", 1.0)
+class ChildVaccinationChecklist {
+    static exec(enrolment, checklistDetails) {
+        const vaccinationDetails = checklistDetails.find(cd => cd.name === 'Vaccination');
+        if (vaccinationDetails === undefined) return [];
+        const vaccinationList = {
+            baseDate: enrolment.individual.dateOfBirth,
+            detail: {uuid: vaccinationDetails.uuid},
+            items: vaccinationDetails.items.map(vi => ({
+                detail: {uuid: vi.uuid}
+            }))
+        };
+        return [vaccinationList];
+    }
+}
 
-module.exports = {CheckListViewFilterChetna};
+
+
+module.exports = {CheckListViewFilterChetna, ChildVaccinationChecklist};
