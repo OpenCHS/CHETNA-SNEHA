70bf9a965c2cb8971843fb46ff8a8edefc53d15d
nupoorkhandelwal <nupoor.khadelwal@gmail.com>
2019-08-19 13:20:44 +0530

    changing org name to match prod

--- create_organisation.sql
+++ create_organisation.sql
@@ -1,6 +1,6 @@
 select create_db_user('chetna-sneha', 'password');
 
 INSERT into organisation(name, db_user, uuid, parent_organisation_id)
-values ('CHETNA-SNEHA', 'chetna-sneha', '28435bdb-0152-4175-a4e6-ed233372d3a7',
+values ('CHETNA SNEHA', 'chetna-sneha', '28435bdb-0152-4175-a4e6-ed233372d3a7',
     (select id FROM organisation WHERE name = 'OpenCHS'))
 ON CONFLICT (uuid) DO NOTHING;
