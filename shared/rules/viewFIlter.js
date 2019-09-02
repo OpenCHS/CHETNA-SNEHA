9eb77f206d2208f36ace7e1c808147d2e57d1941
nupoorkhandelwal <nupoor.khadelwal@gmail.com>
2019-09-02 13:45:28 +0530

    #25 | Added Cancel  form for both the encounter types

--- shared/rules/viewFIlter.js
+++ shared/rules/viewFIlter.js
@@ -3,9 +3,7 @@ import {
     RuleFactory,  
     FormElementsStatusHelper,
     FormElementStatusBuilder,
-    FormElementStatus,
-    WithName,
-    EnrolmentEligibilityCheck
+    WithName
 } from 'rules-config/rules';
 
 const ProgramExitViewFilter = RuleFactory("54cbf339-4cd2-4e26-bf9c-15f128035b1b", "ViewFilter");
@@ -37,4 +35,21 @@ const ProgramExitViewFilter = RuleFactory("54cbf339-4cd2-4e26-bf9c-15f128035b1b"
 }
 
 
-module.exports = {ProgramExitViewFilterHandler};
+const statusBuilder = StatusBuilderAnnotationFactory('programEncounter', 'formElement');
+const filters = RuleFactory("aac5c57a-aa01-49bb-ad20-70536dd2907f", "ViewFilter");
+@filters("538c2ee6-1510-4835-9e66-e09e9f6c92cd", "CancellationFormFilters", 121.0, {})
+class CancellationFormFilters {
+
+    @WithName('Cancel reason')
+    @statusBuilder
+    a11([], statusBuilder) {
+        statusBuilder.skipAnswers("Away from village","Absent");
+    }
+
+    static exec(programEncounter, formElementGroup, today) {
+        return FormElementsStatusHelper.getFormElementsStatusesWithoutDefaults(new CancellationFormFilters(), programEncounter, formElementGroup, today);
+    }
+}
+
+
+module.exports = {ProgramExitViewFilterHandler, CancellationFormFilters};
